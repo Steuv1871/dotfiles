0796b89b0bcbe4763504281a031846ac17de8870
Steuv1871 <stevo447@hotmail.com>
2025-03-30 18:21:20 +0200

    hyperland: hyprland config for Garuda

---
+++ hyprland/.config/hypr/scripts/waybar-workaround.sh
@@ -0,0 +1,2 @@
+# 02/05/2024 Workaround for newest version of hyprland: https://www.reddit.com/r/hyprland/comments/1cig4j2/problem_with_socket_name_being_different_hyprland/
+ln -s $XDG_RUNTIME_DIR/hypr /tmp/hypr
