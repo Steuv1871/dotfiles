2c73f223e26f5796a826614ec366bcca303e8acd
Steuv1871 <stevo447@hotmail.com>
2025-04-06 22:20:08 +0200

    Windows: Change powershell script to manage rerun

--- windows/powershell/bootstrap-powershell.ps1
+++ windows/powershell/bootstrap-powershell.ps1
@@ -3,10 +3,20 @@
 $source = "$PSScriptRoot\WindowsPowerShell"
 $destination = Split-Path -Parent $PROFILE
 
-# Check if the destination exists and rename it if it does
-if ((Test-Path -Path "$destination")) {
-    Rename-Item -Path $destination -NewName "$($destination).bck"
+# Function to check if a directory is a symbolic link
+function Test-ReparsePoint([string]$path) {
+    $file = Get-Item $path -Force -ea SilentlyContinue
+    return [bool]($file.Attributes -band [IO.FileAttributes]::ReparsePoint)
+    }
+
+# Create a symbolic link if the profil is not already a symbolic link
+if (-not (Test-ReparsePoint($destination))) {
+    # Check if the destination exists and rename it if it does
+    if ((Test-Path -Path "$destination")) {
+        Rename-Item -Path $destination -NewName "$($destination).bck"
+    }
+
+    # Create the symbolic link with elevated privileges
+    Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Command `& { New-Item -ItemType SymbolicLink -Path $destination -Target $source }" -Verb RunAs
 }
 
-# Create the symbolic link with elevated privileges
-Start-Process powershell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Command `& { New-Item -ItemType SymbolicLink -Path $destination -Target $source }" -Verb RunAs
