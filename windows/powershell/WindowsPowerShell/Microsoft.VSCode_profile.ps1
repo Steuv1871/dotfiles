495bb5540e44e2b362186277730b86533940c4c5
Steuv1871 <stevo447@hotmail.com>
2026-01-11 12:56:07 +0100

    PWSH: typo in VSCODE profil

--- windows/powershell/WindowsPowerShell/Microsoft.VSCode_profile.ps1
+++ windows/powershell/WindowsPowerShell/Microsoft.VSCode_profile.ps1
@@ -1,3 +1,3 @@
 # VS Code PowerShell profile script
 # Call the Standard PowerShell profile
-. "$HOME\Documents\Microsoft.PowerShell_profile.ps1"
+. "$HOME\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
