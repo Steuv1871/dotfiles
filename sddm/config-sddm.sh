4e797106201922078290c68a795d13704ee2c62c
Steuv1871 <stevo447@hotmail.com>
2026-01-25 17:50:02 +0100

    SDDM: change to avoid stowing package as it is installed by script

--- sddm/config-sddm.sh
+++ sddm/config-sddm.sh
@@ -2,10 +2,10 @@
 # Install sddm themes and config
 
 ## Copy sddm theme
-sudo cp -r ~/.config/sddm/themes /usr/share/sddm/themes/
+sudo cp -r ~/.dotfiles/sddm/themes /usr/share/sddm/themes/
 
 ## Copy faces
-sudo cp -r ~/.config/sddm/faces /usr/share/sddm/faces/
+sudo cp -r ~/.dotfiles/sddm/faces /usr/share/sddm/faces/
 
 ## Copy sddm config
-sudo cp ~/.config/sddm/sddm.conf /etc/sddm.conf
+sudo cp ~/.dotfiles/sddm/sddm.conf /etc/sddm.conf
